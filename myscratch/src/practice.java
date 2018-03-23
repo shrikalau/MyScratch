324cdfc7f9fd69a790c6cbcc1f1a0c75a0703468
shrikalau <37180642+shrikalau@users.noreply.github.com>
2018-03-23 02:29:11 -0700

    changes made to practice.java class

--- myscratch/src/practice.java
+++ myscratch/src/practice.java
@@ -4,8 +4,8 @@ public class practice {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 5; i++) {
-			System.out.println("hello world....");
+		for (int i = 0; i < 3; i++) {
+			
 			System.out.println("hello world....");
 		}
 
