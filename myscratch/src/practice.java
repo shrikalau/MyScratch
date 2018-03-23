de3fb057d8f8dfdc09b58ad98d410535c2c3ab5e
shrikalau <shrikala.upadhya@gmail.com>
2018-03-23 13:45:53 -0700

    addedd a statement

--- myscratch/src/practice.java
+++ myscratch/src/practice.java
@@ -5,7 +5,7 @@ public class practice {
 	public static void main(String[] args) {
 
 		for (int i = 0; i < 5; i++) {
-			
+			System.out.println("hello world....");
 			System.out.println("hello world....");
 		}
 
